ba92772e17631fd0017875927329a98ee3b8b0b1
toma-k <1462053@g.iuhw.ac.jp>
2020-05-18 17:07:59 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by tomakazuki on 2020/05/18.
+//  Copyright © 2020 tomakazuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
